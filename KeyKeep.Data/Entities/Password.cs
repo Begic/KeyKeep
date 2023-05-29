338ca4c038d63b01e9216127922608534b67cf1d
Süleyman Begic <begic@software.tirol>
2023-05-29 17:41:39 +0200

    grid problem

--- KeyKeep.Data/Entities/Password.cs
+++ KeyKeep.Data/Entities/Password.cs
@@ -5,11 +5,11 @@ namespace KeyKeep.Data.Entities;
 public class Password
 {
     public int Id { get; set; }
-    [MaxLength(200)] public string Title { get; set; }
-    [MaxLength(200)] public string Description { get; set; }
+    [MaxLength(200)] public string? Title { get; set; }
+    [MaxLength(200)] public string? Description { get; set; }
     [MaxLength(200)] public string UserPassword { get; set; }
     [MaxLength(200)] public string UserName { get; set; }
-    [MaxLength(200)] public string URL { get; set; }
+    [MaxLength(200)] public string? URL { get; set; }
 
     public User User { get; set; }
     public int UserId { get; set; }
