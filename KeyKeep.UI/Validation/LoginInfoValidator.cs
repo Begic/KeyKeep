2c69703dda387b83be7f98bec641564f579014d9
Süleyman Begic <begic@software.tirol>
2023-06-02 10:48:56 +0200

    implement fluentValidation

---
+++ KeyKeep.UI/Validation/LoginInfoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using KeyKeep.Data.Models;
+
+namespace KeyKeep.UI.Validation;
+
+public class LoginInfoValidator : AbstractValidator<LoginInfo>
+{
+    public LoginInfoValidator()
+    {
+        RuleFor(x => x.Email).NotNull().NotEmpty();
+        RuleFor(x => x.Password).NotNull().NotEmpty();
+    }
+}
