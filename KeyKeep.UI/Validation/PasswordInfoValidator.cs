25abbb3fcaf1626121ea09ae3e84fcabcb6cb77a
Süleyman Begic <begic@software.tirol>
2023-06-02 11:04:43 +0200

    sync validierung

---
+++ KeyKeep.UI/Validation/PasswordInfoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using KeyKeep.Data.Models;
+
+namespace KeyKeep.UI.Validation
+{
+    public class PasswordInfoValidator : AbstractValidator<PasswordInfo>
+    {
+        public PasswordInfoValidator()
+        {
+            RuleFor(x => x.Title).NotNull().NotEmpty();
+            RuleFor(x => x.Description).NotNull().NotEmpty();
+            RuleFor(x => x.URL).NotNull().NotEmpty();
+            RuleFor(x => x.UserName).NotNull().NotEmpty();
+            RuleFor(x => x.UserPassword).NotNull().NotEmpty();
+        }
+    }
+}
---
+++ KeyKeep.UI/Validation/RegisterInfoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using KeyKeep.Data.Models;
+
+namespace KeyKeep.UI.Validation
+{
+    public class RegisterInfoValidator : AbstractValidator<RegisterInfo>
+    {
+        public RegisterInfoValidator()
+        {
+            RuleFor(x => x.FirstName).NotNull().NotEmpty();
+            RuleFor(x => x.LastName).NotNull().NotEmpty();
+            RuleFor(x => x.Email).NotNull().NotEmpty();
+            RuleFor(x => x.LoginPassword).NotNull().NotEmpty().MinimumLength(5);
+        }
+    }
+}
