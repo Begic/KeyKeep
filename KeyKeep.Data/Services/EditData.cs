931ef9348d253d0fa4553157f7601df8463b687d
Süleyman Begic <begic@software.tirol>
2023-06-12 10:32:31 +0200

    start editdata

---
+++ KeyKeep.Data/Services/EditData.cs
@@ -0,0 +1,24 @@
+﻿namespace KeyKeep.Data.Services;
+
+public static class EditData
+{
+    private static void Hash()
+    {
+
+    }
+
+    private static void EnHash()
+    {
+
+    }
+
+    public static void Encrypt()
+    {
+
+    }
+
+    public static void Decrypt()
+    {
+
+    }
+}
